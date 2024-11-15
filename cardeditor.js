272080458db449a2b0748795fdec09469e8e0e8a
IndieGameFanatic <argumentium1@gmail.com>
2024-11-15 18:11:34 +0300

    modified style

--- cardeditor.js
+++ cardeditor.js
@@ -25,6 +25,7 @@ const editImagePositionEvent = (ID, variable) => {
     const positionInputLabel = document.getElementById(`${ID}-label`)
     const Pretext = positionInputLabel.textContent
     positionInputLabel.textContent += "0"
+    positionInput.value = 0
     positionInput.addEventListener("input", function (event) {
         imageValues[variable] = positionInput.value
         positionInputLabel.textContent = `${Pretext}${imageValues[variable]}`
@@ -37,6 +38,7 @@ const editImageScaleEvent = (ID, variable) => {
     const scaleSliderLabel = document.getElementById(`${ID}-label`)
     const Pretext = scaleSliderLabel.textContent
     scaleSliderLabel.textContent += "100%"
+    scaleSlider.input = 2
     scaleSlider.addEventListener("input", function (event) {
         imageValues[variable] = 512.0 * Math.pow(10.0, scaleSlider.value - 2)
         scaleSliderLabel.textContent = `${Pretext}${Math.pow(10.0, scaleSlider.value).toFixed(2)}%`
